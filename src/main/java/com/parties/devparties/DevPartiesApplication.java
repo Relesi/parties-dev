1c29a5f485b965f48bd830a763ac942a443b9e09
Renato Lessa da Silva <renatolessa.2011@hotmail.com>
2018-10-14 10:35:14 -0300

    Referencia modelo WS RESTful com spring boot

---
+++ src/main/java/com/parties/devparties/DevPartiesApplication.java
@@ -0,0 +1,12 @@
+package com.parties.devparties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevPartiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevPartiesApplication.class, args);
+	}
+}
